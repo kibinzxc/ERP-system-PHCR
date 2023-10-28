64cf729254b33167efe05b13cc756e1004dd9567
kevin <mamadaddyalexa@gmail.com>
2023-10-28 08:02:07 +0800

    user login

--- login.php
+++ login.php
@@ -1,39 +1,57 @@
+<?php
+include('src/db/config.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $user_type = $row["user_type"];
+            switch ($user_type) {
+                case "cashier":
+                    header("Location: cashier-dashboard.php");
+                    break;
+                case "manager":
+                    header("Location: manager-dashboard.php");
+                    break;
+                case "customer":
+                    header("Location: ordering-page.php");
+                    break;
+                default:
+                    echo "Invalid User Credentials";
+            }
+        }
+    } else {
+        echo "Invalid username or password. Please try again.";
+    }
+
+    $conn->close();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <link rel="stylesheet" href="styles.css">
+    <title>Login Page</title>
 </head>
 
 <body>
-    <div>
+    <div class="login-container">
         <h2>Login</h2>
-        <form action="process_login.php" method="POST">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" onkeyup="validateUsername()" required><br>
-            <span id="username-status"></span><br>
-
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required><br><br>
-
-            <input type="submit" value="Login">
+        <form action="login.php" method="post">
+            <input type="text" name="email" placeholder="email" required><br>
+            <input type="password" name="password" placeholder="Password" required><br>
+            <button type="submit">Login</button>
         </form>
     </div>
-
-    <script>
-        function validateUsername() {
-            var username = document.getElementById("username").value;
-            var statusElement = document.getElementById("username-status");
-
-            if (username.startsWith("02")) {
-                statusElement.textContent = "✔";
-            } else {
-                statusElement.textContent = "✘";
-            }
-        }
-    </script>
 </body>
 
 </html>
