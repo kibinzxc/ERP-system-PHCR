8e30afb83a3195f9a4a0c5c2de7a259cd6b35eb7
KDA <mamadaddyalexa@gmail.com>
2024-01-23 07:01:23 +0800

    rename index folder to ordering

--- index.php
+++ index.php
@@ -60,13 +60,13 @@ if (isset($_GET['logout'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="src/bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="src/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="src/pages/index/css/styles.css">
+    <link rel="stylesheet" href="src/pages/ordering/css/styles.css">
     <script src="src/bootstrap/js/bootstrap.min.js"></script>
     <script src="src/bootstrap/js/bootstrap.js"></script>
     <script src="https://kit.fontawesome.com/0d118bca32.js" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="src/pages/index/js/index.js"></script>
-    <script src="src/pages/index/js/search-index.js"></script>
+    <script src="src/pages/ordering/js/index.js"></script>
+    <script src="src/pages/ordering/js/search-index.js"></script>
 </head>
 
 <body>
@@ -80,29 +80,29 @@ if (isset($_GET['logout'])) {
                     <a href="index.php" class="item1">
                         <img class="logo" src="src\assets\img\pizzahut-logo.png" alt="Pizza Hut Logo">
                     </a>
-                    <a href="src\pages\index\favorites.php" class="item">
+                    <a href="src\pages\ordering\favorites.php" class="item">
                         <i class="fa-regular fa-heart"></i>
                         <span>Favorites</span>
                     </a>
-                    <a href="src\pages\index\menu.php" class="item">
+                    <a href="src\pages\ordering\menu.php" class="item">
                         <i class="fa-solid fa-utensils"></i>
                         <span>Menu</span>
                     </a>
-                    <a href="src\pages\index\order.php" class="item">
+                    <a href="src\pages\ordering\order.php" class="item">
                         <i class="fa-solid fa-receipt"></i>
                         <span>Order</span>
                     </a>
-                    <a href="src\pages\index\promo.php" class="item">
+                    <a href="src\pages\ordering\promo.php" class="item">
                         <i class="fa-solid fa-ticket"></i>
                         <span>Promo</span>
                     </a>
-                    <a href="src\pages\index\rewards.php" class="item-last">
+                    <a href="src\pages\ordering\rewards.php" class="item-last">
                         <i class="fa-solid fa-trophy"></i>
                         <span>Rewards</span>
                     </a>
                     <!-- Toggle Login/Logout link -->
                     <?php if ($loggedIn): ?>
-                        <a href="src\pages\index\profile.php" class="item">
+                        <a href="src\pages\ordering\profile.php" class="item">
                             <i class="fa-solid fa-user"></i>
                             <span>Profile</span>
                         </a>
